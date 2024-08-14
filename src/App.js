bdbd01f20760b035f3d56a041c035b7bf0d28ee4
Hellings <hellingsmakondetsa@gmail.com>
2024-08-14 03:48:22 +0200

    Project underway

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1 id='logo-text'>Budgie.</h1
+      </div>
+      <div className='main'>
+        <div className='grid-item'></div>
+        <div className='grid-item'></div>
+        <div className='grid-item'>
+          <h2>1,000,000,000</h2>
+        </div>
+      </div>
     </div>
   );
 }
