7f0fbcf9d0449f87ccb81060644c5165c38c8d50
Hellings <hellingsmakondetsa@gmail.com>
2024-08-16 06:17:21 +0200

    foundation built

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        <h1 id='logo-text'>Budgie.</h1
+        <h1 id='logo-text'>Budgie.</h1>
       </div>
       <div className='main'>
         <div className='grid-item'></div>
-        <div className='grid-item'></div>
         <div className='grid-item'>
-          <h2>1,000,000,000</h2>
+          <div className='transact'>
+            <button id='deposit'>Deposit</button>
+            <button id='withdraw'>Withdraw</button>
+          </div>
+          
+        </div>
+        <div className='grid-item'>
+          <h2 id="balance">1,000,000,000</h2>
+          <div className='pockets'>
+            <ul>
+              <li>Cash</li>
+              <li>Mpamba</li>
+              <li>Airtel Money</li>
+              <li>Bank</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
