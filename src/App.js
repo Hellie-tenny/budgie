a8ea1c283b6e7beef83cc5693733cc5247d64538
Hellings <hellingsmakondetsa@.com>
2024-11-04 02:18:12 +0200

    working prototype

--- src/App.js
+++ src/App.js
@@ -5,51 +5,72 @@ import Footer from './components/Footer';
 
 
 
+
 function App() {
 
-  // states and variables
-  // const [cashBalance, setCashBalance] = useState(0);
-  // const [mpambaBalance, setMpambsBalance] = useState(0);
-  // const [bankBalance, setBankBalance] = useState(0);
-  // const [airtelMoneyBalance, setairtelMoneyBalance] = useState(0);
+  // states and other variables
   const [totalBalance, setTotalBalance] = useState(0);
-  const [balanceList, setBalanceList] = useState([]);
+  const [pockets, setPockets] = useState([]);
+  let storedPockets = [];
 
-  // object approach
-  const [pockets, setPockets] = useState([
-    {
-      name: "cash",
-      value: 400
-    },
-    {
-      name: "bank",
-      value: 360
-    },
-    {
-      name: "airtelmoney",
-      value: 960
-    },
-    {
-      name: "mpamba",
-      value: 5000
+  useEffect(() => {
+    if (!localStorage.getItem("pockets")) {
+      // object approach sample array of objects
+      setPockets(
+        [
+          {
+            name: "cash",
+            value: 0
+          },
+          {
+            name: "bank",
+            value: 0
+          },
+          {
+            name: "airtel money",
+            value: 0
+          },
+          {
+            name: "mpamba",
+            value: 0
+          }
+        ]
+      );
+
+      console.log("The local storage was empty, therfore pockets were set to detault value!", pockets);
+
+    } else {
+      setPockets(JSON.parse(localStorage.getItem("pockets")));
+      console.log("The local storage was found, this is the value", pockets)
     }
-  ]);
+  }, [])
 
 
   useEffect(() => {
-
-    const total = pockets.reduce((acc, pocket) => {
-      return acc + pocket.value;
-    }, 0);
-
-    setTotalBalance(total);
-    console.log("POCKETS HAS CHANGED!!!!!!!!!!!!")
-
+    updateBalance();
+    console.log("use effect activated!");
   }, [pockets]);
 
   const showDepositForm = () => {
     document.getElementById('deposit-container').style.display = 'flex';
 
+  }
+
+  // this function updates pockets
+  const updatePockets = () => {
+    // updating balances
+    setPockets(JSON.parse(localStorage.getItem("pockets")));
+  }
+
+  // this function updates the total balance
+  const updateBalance = () => {
+    const total = pockets.reduce((acc, pocket) => {
+      return acc + pocket.value;
+      console.log("Balance is set")
+    }, 0);
+
+    setTotalBalance(total);
+    console.log("POCKETS HAS CHANGED!!!!!!!!!!!!");
   }
 
   return (
